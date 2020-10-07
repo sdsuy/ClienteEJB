916a30560c38ee26349862ecb04a27ce0593948f
Santiago Da Silva <sdasilva@outlook.es>
2020-10-06 22:14:26 -0300

    Cliente main

--- src/cliente/ClienteSaludo.java
+++ src/cliente/ClienteSaludo.java
@@ -1,10 +1,17 @@
 package cliente;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import servicios.SaludoBeanRemote;
+
 public class ClienteSaludo {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws NamingException {
+		String context = "ejb:/ServidorEJB/SaludoBean!servicios.SaludoBeanRemote";
+		SaludoBeanRemote saludoBean = (SaludoBeanRemote)InitialContext.doLookup(context);
+		System.out.println(saludoBean.saludar("Santiago Da Silva"));
+		System.out.println("Excelente!");
 	}
 
 }
